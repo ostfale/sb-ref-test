dadfb7d355ac53516a7bbdc8d52a5b18fcb69515
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2024-11-07 07:47:37 +0100

    Initial Commit

---
+++ src/main/java/de/ostfale/sb/sbreftest/SbRefTestApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.sb.sbreftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRefTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRefTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/sb/sbreftest/SbRefTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.sb.sbreftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRefTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
